16b1c3c07194f4c7cd43227d64a01f8e945c5afb
ffalafluke43 <ffalafluke43@gmail.com>
2022-03-02 12:57:11 +0700

    C7_Lab2

---
+++ Assets/ChangeAnimate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+   public void getAnimatorHipHop(){
+       this.GetComponent<Animator>().Play("Bboy Hip Hop Move");
+   }
+
+   public void getAnimatorSurprised(){
+       this.GetComponent<Animator>().Play("Surprised");
+   }
+
+   public void getAnimatorRun(){
+       this.GetComponent<Animator>().Play("Running Backward");
+   }
+
+   
+}
